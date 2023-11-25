c0dbf9172e4c9df80682ab1009f53c6f7b94a945
Hawksley Mouquet <hawksley.mqt@gmail.com>
2023-11-25 19:17:09 +0100

    add projects section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './project.scss'
 
 export default function Project() {
   return (
-    <div>Project</div>
+    <>
+      <div className='project'>
+        <img className='project__image' src="./src/assets/img/pexels-l-m-19031635.jpg" alt="" />
+        <div className='project__info'>
+          <h3 className='project__info__title'>Title</h3>
+          <div className='project__info__tag'></div>
+          <p className='project__info__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci modi quam fugiat repellat obcaecati autem beatae.</p>
+        </div>
+      </div>
+    </>
   )
 }
--- src/layouts/Projects/Projects.jsx
+++ src/layouts/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './projects.scss';
+import Project from '../../components/Project/Project';
 
 export default function Projects() {
   return (
@@ -7,7 +8,10 @@ export default function Projects() {
         <section id='projects' className='projects-section'>
             <h2>Mes p<span className='sp-regular'>rr</span>ojets</h2>
             <div className='projects-grid'>
-                
+                <Project></Project>
+                <Project></Project>
+                <Project></Project>
+                <Project></Project>
             </div>
         </section>
     </>
